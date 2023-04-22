68b8b6c4c11ee858b349e1d4e10cbb4cd01c15a3
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-04-22 23:21:23 +0000

    feat: Add dynamic url map

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+    A script that starts a flask web application
+    and routes it to different urls
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello():
+    """Return the page for this path"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnb():
+    """Return the page for this path"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', methods=['GET'], strict_slashes=False)
+def display_c(text):
+    """Return the page for this path"""
+    ans = text.replace('_', " ")
+    return "C %s" % text
+
+
+@app.route('/python', methods=['GET'], strict_slashes=False)
+@app.route('/python/<string:text>', methods=['GET'], strict_slashes=False)
+def display_py(text="is cool"):
+    """Return the page for this path"""
+    ans = text.replace('_', " ")
+    return "Python %s" % ans
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
